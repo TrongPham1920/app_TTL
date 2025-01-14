4a8a409c2ebe91bbac97c4aa6c06bdb0cf53ff21
TaKieuLong <takieulong@gmail.com>
2025-01-14 23:17:26 +0700

    hot

--- src/screens/Hot/view/ShowAllScreen.jsx
+++ src/screens/Hot/view/ShowAllScreen.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import CustomList from "../../../components/foundation/list/CustomList";
 import useHotModal from "../viewmodal/HotModal";
-
+import DateInput from "../../../components/foundation/date/DateHome";
 const ShowAllScreen = ({ navigation }) => {
-  const { accommodationData, loading } = useHotModal();
+  const { accommodationData, loading, onDateChange, dates } = useHotModal();
 
   return (
     <View style={styles.container}>
+      <View style={styles.inputWrapper}>
+        <DateInput
+          title="Ngày đặt phòng"
+          startDate={dates.fromDate}
+          endDate={dates.toDate}
+          onDateChange={onDateChange}
+        />
+      </View>
       <CustomList
         loading={loading}
         data={accommodationData}
--- src/screens/Hot/viewmodal/HotModal.js
+++ src/screens/Hot/viewmodal/HotModal.js
@@ -1,6 +1,11 @@
 import { useState, useEffect } from "react";
 import { accommodationuser } from "../../../api/app/app.js";
+import dayjs from "dayjs";
 
+const currentDate = dayjs().add(1, "day");
+const defaultStartDate = currentDate.add(1, "day");
+const StartDate = defaultStartDate.format("DD/MM/YYYY");
+const date = currentDate.format("DD/MM/YYYY");
 const useHotModal = () => {
   const [accommodationData, setAccommodationData] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -8,9 +13,16 @@ const useHotModal = () => {
   const [page, setPage] = useState(0);
   const [pageSize, setPageSize] = useState(20);
 
+  const [dates, setDates] = useState({
+    fromDate: date,
+    toDate: StartDate,
+  });
+
   const [filterParams, setFilterParams] = useState({
     page: page,
     limit: pageSize,
+    fromDate: dates.fromDate,
+    toDate: dates.toDate,
   });
 
   const fetchData = async (filterParams) => {
@@ -26,11 +38,29 @@ const useHotModal = () => {
     }
   };
 
+  const onDateChange = (value, name) => {
+    if (name === "endDate") {
+      setFilterParams({
+        ...filterParams,
+        toDate: value,
+      });
+      const newDates = { ...dates, toDate: value };
+      setDates(newDates);
+    } else {
+      setFilterParams({
+        ...filterParams,
+        fromDate: value,
+      });
+      const newDates = { ...dates, fromDate: value };
+      setDates(newDates);
+    }
+  };
+
   useEffect(() => {
     fetchData(filterParams);
   }, [filterParams]);
 
-  return { accommodationData, loading };
+  return { accommodationData, loading, date, dates, onDateChange };
 };
 
 export default useHotModal;
